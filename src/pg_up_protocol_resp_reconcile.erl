ad2f2198814f4894119810ef1ca096ed18991157
simonxu72 <simon.xu72@gmail.com>
2017-11-30 14:34:52 +0800

    remove unused pg_model behaviour define

--- src/pg_up_protocol_resp_reconcile.erl
+++ src/pg_up_protocol_resp_reconcile.erl
@@ -11,7 +11,6 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("mixer/include/mixer.hrl").
 -compile({parse_trans, exprecs}).
--behaviour(pg_model).
 -behavior(pg_protocol).
 -behaviour(pg_up_protocol).
 
